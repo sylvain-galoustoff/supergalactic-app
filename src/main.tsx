a5641bfe92bdc8be431521f4aa3a06de773d45de
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-08-09 13:33:33 +0200

    import sass files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import WebFont from "webfontloader";
+import "./styles/index.scss";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+WebFont.load({
+  google: {
+    families: ["Overpass:400,900"],
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

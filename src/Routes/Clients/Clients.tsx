61bed7a14c83ca44b9584369ab09cad4f99efc63
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-08-18 11:37:15 +0200

    Format clients data for DataSearch

--- src/Routes/Clients/Clients.tsx
+++ src/Routes/Clients/Clients.tsx
@@ -8,11 +8,13 @@ import { useSelector } from "react-redux";
 import { DataSearch, DataTable, useToast } from "simplegems";
 import { ClientType } from "../../models/client";
 import { deleteClient, updateClient } from "../../api/clientApi";
+import { useEffect, useState } from "react";
 
 function Clients() {
   const clientsData = useSelector((state: RootState) => state.clients);
-
+  const [termsPool, setTermsPool] = useState<string[]>([]);
   const sendToast = useToast();
+
   const clientsColumns = ["clientName", "contactName", "contactMail", "contactTel"];
   const clientsLabels = {
     clientName: "Nom du client",
@@ -20,6 +22,17 @@ function Clients() {
     contactMail: "Email du contact",
     contactTel: "Téléphone du contact",
   };
+
+  useEffect(() => {
+    const clientsDataValues: string[] = [];
+    clientsData.forEach(({ uid, id, ...client }) => {
+      Object.values(client).map((value) => {
+        if (value !== "") clientsDataValues.push(value);
+      });
+    });
+    const pool = Array.from(new Set(clientsDataValues));
+    setTermsPool(pool);
+  }, [clientsData]);
 
   const handleDelete = async (value: { [key: string]: string }) => {
     const response = await deleteClient(value as Record<keyof ClientType, string>);
@@ -47,9 +60,7 @@ function Clients() {
     <main id="clients">
       <PageHeader title="Clients" />
       <Toolbar
-        left={
-          <DataSearch callback={handleTerm} data={clientsColumns} id="table-search" />
-        }
+        left={<DataSearch callback={handleTerm} data={termsPool} id="table-search" />}
         right={
           <ModalButton
             label="Ajouter un client"

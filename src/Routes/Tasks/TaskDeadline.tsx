0043cc95664c96460418493a6ebc2672bf57528d
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-08-20 14:13:49 +0200

    TaskDeadline done

--- src/Routes/Tasks/TaskDeadline.tsx
+++ src/Routes/Tasks/TaskDeadline.tsx
@@ -1,11 +1,38 @@
+import { differenceInDays } from "date-fns";
+import { useEffect, useState } from "react";
+
 export type TaskDeadlineProps = {
   deadline: number | null;
 };
 
 function TaskDeadline({ deadline }: TaskDeadlineProps) {
-  console.log(deadline);
+  const [difference, setDifference] = useState(0);
+  const [emergency, setEmergency] = useState<"success" | "warning" | "danger">("success");
 
-  return <p className="task-deadline help danger">Dans 4 jours</p>;
+  useEffect(() => {
+    if (deadline) {
+      const daysLeft = differenceInDays(new Date(deadline), Date.now());
+      setDifference(daysLeft);
+
+      const emergencyStatus = () => {
+        if (daysLeft <= 0) {
+          return "danger";
+        } else if (daysLeft <= 10) {
+          return "warning";
+        } else if (daysLeft >= 11) {
+          return "success";
+        } else {
+          return "success";
+        }
+      };
+
+      setEmergency(emergencyStatus());
+    }
+  }, [deadline]);
+
+  return (
+    <p className={`task-deadline help black ${emergency}`}>Reste {difference} jours</p>
+  );
 }
 
 export default TaskDeadline;
